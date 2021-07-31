ad51be929c7f34d198b58692a2b156dff1bf1bff
Gleison Batista Soares <gleisonbs@gmail.com>
2021-07-31 15:52:09 -0300

    feat: load web3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import Web3 from 'web3'
+import Voting from './abis/Voting.json';
 import './App.css';
 
+
 function App() {
+
+  const loadWeb3 = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      alert('Ethereum wallet not found');
+    }
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
