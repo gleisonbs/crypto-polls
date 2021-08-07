5c4adcb50484786706da79714eccb9c6da8ff8e4
Gleison Batista Soares <gleisonbs@gmail.com>
2021-08-06 22:05:13 -0300

    feat: add poll creation

--- contracts/PollCreation.sol
+++ contracts/PollCreation.sol
@@ -1,9 +1,13 @@
-pragma solidity ^0.5.0;
+pragma solidity 0.8.0;
 
 contract PollCreation {
+    uint256 public totalPolls = 0;
+    mapping(uint256 => address) public pollToOwner;
+    mapping(uint256 => Poll) public polls;
     mapping(uint256 => string[]) pollOptions;
     mapping(uint256 => uint256[]) pollVotes;
 
+    //TODO: REDUCE UINT SIZE
     struct Poll {
         uint256 id;
         uint256 voteCount;
@@ -13,5 +17,16 @@ contract PollCreation {
         string title;
     }
 
-    function create() public {}
+    function createPoll(string memory _title) public {
+        totalPolls++;
+        pollToOwner[totalPolls] = msg.sender;
+        polls[totalPolls] = Poll(
+            totalPolls,
+            0,
+            block.timestamp,
+            block.timestamp,
+            msg.sender,
+            _title
+        );
+    }
 }
--- test/poll.test.js
+++ test/poll.test.js
@@ -3,8 +3,9 @@ const { default: Web3} = require('web3');
 
 const PollCreation = artifacts.require('../contracts/PollCreation.sol');
 
-contract('PollCreation', () => {
+contract('PollCreation', ([deployer, author]) => {
   let poll;
+  const title = "test.title"
   describe('Deployment', () => {
     before(async () => {
       poll = await PollCreation.deployed();
@@ -17,6 +18,25 @@ contract('PollCreation', () => {
       assert.notEqual(address, '');
       assert.notEqual(address, null);
       assert.notEqual(address, undefined);
+    })
+
+    it('Creates a poll', async () => {
+      let totalPolls = await poll.totalPolls();
+      assert.equal(totalPolls.toNumber(), 0, "starts with totalPolls set to 0");
+      
+      await poll.createPoll(title, { from: author });
+
+      totalPolls = await poll.totalPolls();
+      assert.equal(totalPolls.toNumber(), 1, "increments totalPolls to 1 after creating poll");
+
+      const pollOwner = await poll.pollToOwner(1);
+      assert.equal(pollOwner, author, "pollOwner is the author");
+
+      const userPoll = await poll.polls(1);
+      assert.equal(userPoll.id.toNumber(), 1, "id is correct");
+      assert.equal(userPoll.author, author, "author is correct");
+      assert.equal(userPoll.voteCount, 0, "vote count is correct");
+      assert.equal(userPoll.title, title, "title is correct");
     })
   })
 })
