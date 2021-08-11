072af14bdb8f0bd4106bdba1ba92152eedd93218
Gleison Batista Soares <gleisonbs@gmail.com>
2021-08-10 23:05:26 -0300

    feat: add datetime picker component for poll close date

--- src/components/CreatePoll.js
+++ src/components/CreatePoll.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const CreatePoll = ({ cryptoPolls, account }) => {
   const [title, setTitle] = useState('');
+  const [closeDate, setCloseDate] = useState(null);
   const [option, setOption] = useState('');
   const [options, setOptions] = useState([]);
 
   const handleCreatePoll = async () => {
-    console.log(title);
-    const createdPoll = await cryptoPolls.methods.createPoll(title, options.length, options).send({ from: account });
+    const closeDateInSeconds = Math.floor(closeDate.getTime() / 1000);
+    const createdPoll = await cryptoPolls.methods
+      .createPoll(title, options.length, options, closeDateInSeconds)
+      .send({ from: account });
     console.log(createdPoll);
     const _totalPolls = await cryptoPolls.methods.totalPolls().call();
     console.log(_totalPolls);
@@ -38,7 +43,18 @@ const CreatePoll = ({ cryptoPolls, account }) => {
           <Form.Input label="Add an Option:" type="text" value={option} onChange={(e) => setOption(e.target.value)} />
           <Button type="button" onClick={handleAddOption}>+</Button>
         </Form.Field>
+        <Form.Field>
+          <label>Close Date:</label>
+          <DatePicker
+            selected={closeDate}
+            onChange={setCloseDate}
+            timeInputLabel="Time:"
+            timeFormat="HH:mm"
+            dateFormat="MM/dd/yyyy, HH:mm"
+            showTimeInput
+          />
         <Button type="button" onClick={handleCreatePoll}>Create</Button>
+        </Form.Field>
       </Form>
     </Segment>
     </div>
